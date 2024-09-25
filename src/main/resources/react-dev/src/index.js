ec1dc471d6e28f55d023585035d318ab89df04fa
purin <k24555569@gmail.com>
2024-09-25 21:08:00 +0900

    fix css js path

--- src/main/resources/react-dev/src/index.js
+++ src/main/resources/react-dev/src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './static/css/index.css';
-import App from './static/js/App.js';
+import '/static/css/index.css';
+import App from '/static/js/App.js';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
